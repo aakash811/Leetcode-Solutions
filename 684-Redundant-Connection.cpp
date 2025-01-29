78a875919b0331b20a9e7cbbb0698fe324ea774e
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-01-29 23:47:19 +0530

    [684] [Time Beats: 100.00%] [Memory Beats: 93.68%] - LeetPush

---
+++ 684-Redundant-Connection.cpp
@@ -0,0 +1,38 @@
+class UnionFind { 
+    vector<int> root, rank;
+public:
+    UnionFind(int n) : root(n), rank(n) {
+        rank.assign(n, 1);
+        iota(root.begin(), root.end(), 0);
+    }
+
+    int Find(int x) {
+        if (x == root[x])
+            return x;
+        else
+            return root[x] = Find(root[x]);
+    }
+
+    bool Union(int x, int y) {
+        int rX = Find(x), rY = Find(y);
+        if (rX == rY)
+            return 0;
+        if (rank[rX] > rank[rY])
+            swap(rX, rY);
+        root[rX] = rY;
+        if (rank[rX] == rank[rY])
+            rank[rY]++;
+        return 1;
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        const int n=edges.size();
+        UnionFind G(n+1);
+        for(auto& e: edges)
+            if (!G.Union(e[0], e[1])) return e;
+        return {};
+    }
+};
