37eea7ad6e34a1038440230ee6b2516a62bbe661
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-02-04 19:55:12 +0530

    [2149] [Time Beats: 92.62%] [Memory Beats: 42.58%] - LeetPush

---
+++ 2149-Rearrange-Array-Elements-by-Sign.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>res(n, 0);
+
+        int idx1 = 0, idx2 = 1;
+
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] > 0)
+            {
+                res[idx1] = nums[i];
+                idx1 += 2;
+            }
+            else
+            {
+                res[idx2] = nums[i];
+                idx2 += 2;
+            }
+        }
+        return res; 
+    }
+};
