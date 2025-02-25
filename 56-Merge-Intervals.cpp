97412a3ab957c9875556f4d1cfb90df6150411db
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-02-26 01:45:03 +0530

    [56] [Time Beats: 44.19%] [Memory Beats: 35.06%] - LeetPush

---
+++ 56-Merge-Intervals.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>>res;
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(res.empty() || res.back()[1] < intervals[i][0])
+            {
+                res.push_back(intervals[i]);
+            }
+            else
+            {
+                res.back()[1] = max(intervals[i][1], res.back()[1]);
+            }
+        }
+        return res;
+    }
+};
