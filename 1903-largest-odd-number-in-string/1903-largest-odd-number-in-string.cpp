cf5508f5a3e21ff9688e6c72414d85fcfab136e2
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-03-14 23:00:10 +0530

    Time: 14 ms (5.08%), Space: 19.3 MB (5.3%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -5,9 +5,10 @@ public:
         string maxOdd = "";
 
         int idx = -1;
-        for(int i = 0; i < n; i++){
+        for(int i = n - 1; i >= 0; i--){
             if(num[i] == '1' || num[i] == '3' || num[i] == '5' || num[i] == '7' || num[i] == '9'){
                 idx = i;
+                break;
             }
         }
 
