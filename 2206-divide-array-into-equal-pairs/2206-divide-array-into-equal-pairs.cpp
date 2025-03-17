d2994505debce7b666b65bc0e02fa5f05b4921fa
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-03-17 16:41:30 +0530

    Time: 6 ms (24.13%), Space: 18.2 MB (31.79%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -5,11 +5,17 @@ public:
         unordered_map<int,int>ump;
         int divs = n / 2;
         
+        // cout<<n<<" "<<divs<<endl;
         for(int i = 0; i < n; i++){
             ump[nums[i]]++;
         }
+        // cout<<ump.size()<<endl;
+        int pairs = 0;
+        for(auto it : ump){
+            pairs += (it.second + 1) / 2;
+        }
 
-        if(ump.size() > divs){
+        if(pairs != divs){
             return false;
         }
         return true;
