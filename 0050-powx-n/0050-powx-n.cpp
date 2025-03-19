b89a71ef8bc7422fec3bd0a8bfad7829538cd765
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-03-19 19:25:57 +0530

    Time: 0 ms (100%), Space: 8.7 MB (15.65%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double binaryExpo(double x, long n){
+        if(n == 0){
+            return 1;
+        }
+
+        if(n < 0){
+            return 1.0 / binaryExpo(x, -n);
+        }
+
+        if(n % 2 != 0){
+            return x * binaryExpo(x * x, (n - 1) / 2);
+        } 
+        else{
+            return binaryExpo(x * x, n / 2);
+        } 
+    }
+    double myPow(double x, int n) {
+        return binaryExpo(x, static_cast<long>(n));
+    }
+};
