b2b04f1fbbbab09ec5fee12e53aee7cd573a33c5
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-03-19 19:26:41 +0530

    Time: 0 ms (100%), Space: 8.7 MB (15.65%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -6,17 +6,16 @@ public:
         }
 
         if(n < 0){
-            return 1.0 / binaryExpo(x, -n);
+            return 1.0/binaryExpo(x, -n);
         }
-
         if(n % 2 != 0){
             return x * binaryExpo(x * x, (n - 1) / 2);
-        } 
+        }
         else{
             return binaryExpo(x * x, n / 2);
-        } 
+        }
     }
     double myPow(double x, int n) {
-        return binaryExpo(x, static_cast<long>(n));
+        return binaryExpo(x, 1LL * n);
     }
 };
