8fb2a2493afa510739b204f989466cf861f4b68d
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-03-20 21:31:05 +0530

    Time: 0 ms (100%), Space: 14.1 MB (73.7%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    void solve(int ind, int target,vector<int>& candidates, vector<int>& ds, vector<vector<int>>& res)
+    {
+        if(ind == candidates.size())
+        {
+            if(target == 0)
+            {
+                res.push_back(ds);
+            }
+            return;
+        }
+
+        if(candidates[ind] <= target)
+        {
+            ds.push_back(candidates[ind]);
+            solve(ind, target - candidates[ind], candidates, ds, res);
+            ds.pop_back();
+        }
+        solve(ind + 1, target, candidates, ds, res);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>res;
+        vector<int> ds;
+        solve(0, target, candidates, ds, res);
+        return res;
+    }
+};
