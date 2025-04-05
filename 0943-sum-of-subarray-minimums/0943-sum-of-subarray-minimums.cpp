0377c9ad1dc00ec010c881d5ee6a1dbc6669b8fa
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-04-05 20:04:01 +0530

    Time: 11 ms (88.73%), Space: 47.3 MB (44.29%) - LeetHub

---
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    const int MOD = 1000000007;
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        stack<int>stk1;
+        stack<int>stk2;
+        vector<int>lft(n);
+        vector<int>rght(n);
+
+        for(int i = 0; i < n; i++){
+            while(!stk1.empty() && arr[stk1.top()] > arr[i]){
+                stk1.pop();
+            }
+            lft[i] = stk1.empty() ? -1 : stk1.top();
+            stk1.push(i);
+        }
+
+        for(int i = n - 1; i >= 0; i--){
+            while(!stk2.empty() && arr[stk2.top()] >= arr[i]){
+                stk2.pop();
+            }
+            rght[i] = stk2.empty() ? n : stk2.top();
+            stk2.push(i);
+        }
+
+        long long res = 0;
+        for(int i = 0; i < n; i++){
+            long long lftSum = i - lft[i];
+            long long rghtSum = rght[i] - i;
+
+            long long sum = (arr[i] * lftSum * rghtSum) % MOD;
+            res = (res + sum) % MOD;
+        }
+
+        return res;
+    }
+};
