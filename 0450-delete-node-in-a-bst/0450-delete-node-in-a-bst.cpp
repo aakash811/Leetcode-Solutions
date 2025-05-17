8e49dcd2b4b8262362ef4a2aa1242fbe858e9620
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-05-17 18:55:07 +0530

    Time: 0 ms (100%), Space: 34.4 MB (29.48%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -1,36 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* findMin(TreeNode* node) {
-        while (node->left != nullptr) {
-            node = node->left;
+    TreeNode* solve(TreeNode* root, int key){
+        while(root->left != nullptr){
+            root = root->left;
         }
-        return node;
+        return root;
     }
 
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if (!root) return nullptr;
+        if(!root){
+            return nullptr;
+        }
 
-        if (key < root->val) {
-            root->left = deleteNode(root->left, key);
-        } else if (key > root->val) {
+        if(root->val < key){
             root->right = deleteNode(root->right, key);
-        } else {
-            // Node to delete found
-            if (!root->left && !root->right) {
+        }
+        else if(root->val > key){
+            root->left = deleteNode(root->left, key);
+        }
+        else{
+            if(root->right == nullptr && root->left == nullptr){
                 delete root;
                 return nullptr;
-            } else if (!root->left) {
+            }
+            else if(root->left == nullptr){
                 TreeNode* temp = root->right;
                 delete root;
                 return temp;
-            } else if (!root->right) {
+            }
+            else if(root->right == nullptr){
                 TreeNode* temp = root->left;
                 delete root;
                 return temp;
-            } else {
-                TreeNode* successor = findMin(root->right);
-                root->val = successor->val;
-                root->right = deleteNode(root->right, successor->val);
+            }
+            else{
+                TreeNode* mini = solve(root -> right, key);
+                root -> val = mini-> val;
+                root -> right = deleteNode(root->right, mini->val);
             }
         }
         return root;
