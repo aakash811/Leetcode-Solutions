6761b1d7489373dbc35f565c7b3199c59d17f4f9
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-05-21 18:54:50 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -4,16 +4,18 @@ public:
         if(n <= 3){
             return n;
         }
-        
+
         int prev1 = 3;
         int prev2 = 2;
+
         int cur = 0;
-        
+
         for(int i = 3; i < n; i++){
             cur = prev1 + prev2;
             prev2 = prev1;
             prev1 = cur;
         }
-            return cur;
+
+        return cur;
     }
 };
