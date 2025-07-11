cefaf0bdbd4dc8fdcaaf278189c5b304e45fffbb
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-12 00:24:48 +0530

    Time: 0 ms (100%), Space: 15.6 MB (12.47%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,46 +1,48 @@
 class Solution {
 public:
-    
-    
-    // void insertion(vector<int>nums){
+    void solve(vector<int>&nums,int s,int mid,int e){
         
-    //     int n=nums.size();
-    //     for(int i=0;i<n;i++){
-            
-    //         int key=0;
-    //         for(int j=i;j>i;i--){
-                
-    //         }
-            
-    //     }
+        int i=s;
+        int j=mid+1;
         
-        
-        
-    // }
-    
-    void bubbleSort(vector<int>& nums){
-        int n = nums.size();
-        
-        // for(int i = n - 1; i >= 0; i--){
-        //     for(int j = 0; j <= i - 1; j++){
-        //         if(nums[j] > nums[j + 1]){
-        //             swap(nums[j], nums[j + 1]);
-        //         }
-        //     }
-        // }
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(nums[j]>nums[j+1]){
-                    swap(nums[j],nums[j+1]);
-                }
+        vector<int>ans;
+        while(i<=mid && j<=e){
+            if(nums[i]<=nums[j]){
+                ans.push_back(nums[i]); 
+                i++; 
             }
+            else{
+                ans.push_back(nums[j]);
+                j++;
+            }
+        }
+        
+        while(i<=mid){
+            ans.push_back(nums[i]);
+            i++;
+        }
+        while(j<=e){
+            ans.push_back(nums[j]);
+            j++;
+        }
+        
+        for(int k=s;k<=e;k++){
+            nums[k]=ans[k-s];
         }
     }
     
-        void sortColors(vector<int>& nums) {
-            
-            bubbleSort(nums);
+    void mergeSort(vector<int>&nums,int s,int e){
+        if(s < e){
+              int mid=s+(e-s)/2;
+              mergeSort(nums,s,mid);
+              mergeSort(nums,mid+1,e);
+              solve(nums,s,mid,e);
         }
-   
+    }
+    
+    void sortColors(vector<int>& nums) {
+     
+        int n=nums.size();
+        return mergeSort(nums,0,n-1);
+    }
 };
