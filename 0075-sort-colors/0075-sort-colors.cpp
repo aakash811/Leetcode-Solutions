c3ce79e61dfc229d76a968a2f846f5397b745c4d
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-12 00:06:55 +0530

    Time: 0 ms (100%), Space: 11.5 MB (97.4%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -21,21 +21,21 @@ public:
     void bubbleSort(vector<int>& nums){
         int n = nums.size();
         
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = 0; j <= i - 1; j++){
-                if(nums[j] > nums[j + 1]){
-                    swap(nums[j], nums[j + 1]);
-                }
-            }
-        }
-        
-        // for(int i=0;i<n;i++){
-        //     for(int j=i;j<n-i-1;j++){
-        //         if(nums[j]>nums[j+1]){
-        //             swap(nums[j],nums[j+1]);
+        // for(int i = n - 1; i >= 0; i--){
+        //     for(int j = 0; j <= i - 1; j++){
+        //         if(nums[j] > nums[j + 1]){
+        //             swap(nums[j], nums[j + 1]);
         //         }
         //     }
         // }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j],nums[j+1]);
+                }
+            }
+        }
     }
     
         void sortColors(vector<int>& nums) {
