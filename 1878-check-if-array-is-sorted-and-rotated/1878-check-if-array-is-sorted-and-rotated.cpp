cad7c13463786bb455db7fa6f3cfe349eb510468
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-14 00:17:31 +0530

    Time: 0 ms (100%), Space: 11.1 MB (96.21%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -4,10 +4,14 @@ public:
         int n = nums.size();
         int cnt = 0;
 
-        for(int i = 0; i < n; i++){
-            if(nums[i] > nums[(i + 1) % n]){
+        for(int i = 0; i < n - 1; i++){
+            if(nums[i] > nums[i + 1]){
                 cnt++;
             }
+        }
+
+        if(nums[n - 1] > nums[0]){
+            cnt++;
         }
         return cnt <= 1;
     }
