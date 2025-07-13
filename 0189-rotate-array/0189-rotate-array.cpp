398a79dbb057ee9e23e594282b0aba2f367a7a9c
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-14 00:27:53 +0530

    Time: 0 ms (100%), Space: 30.5 MB (16.79%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;
         vector<int>res(n);
+        k %= n;
         for(int i = 0; i < n; i++){
             res[(i + k) % n] = nums[i];
         }
