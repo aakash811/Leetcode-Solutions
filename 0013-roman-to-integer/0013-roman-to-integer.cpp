0cc15f1ac6b1beb1676e5454e9b472f86d881ed9
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-15 01:53:38 +0530

    Time: 12 ms (17.74%), Space: 12.4 MB (57.04%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int>mp = {{'I', 1}, {'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000},};
+        unordered_map<char,int>mpp;
         
-        int n = s.size();
-        int num = 0;
-        for(int i = 0; i < n; i++){
-            if(s[i] == 'I' && s[i + 1] == 'V' && i < n - 1){
-                num += 4;
-                i++;
+        mpp['I']=1;
+        mpp['V']=5;
+        mpp['X']=10;
+        mpp['L']=50;
+        mpp['C']=100;
+        mpp['D']=500;
+        mpp['M']=1000;
+        
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            ans+=mpp[s[i]];
+            
+            if(i>0){
+                if(mpp[s[i]]>mpp[s[i-1]]){
+                    ans-=2*mpp[s[i-1]];
+                }
             }
-            else if(s[i] == 'I' && s[i + 1] == 'X' && i < n - 1){
-                num += 9;
-                i++;
-            }
-            else if(s[i] == 'X' && s[i + 1] == 'L' && i < n - 1){
-                num += 40;
-                i++;
-            }
-            else if(s[i] == 'X' && s[i + 1] == 'C' && i < n - 1){
-                num += 90;
-                i++;
-            }
-            else if(s[i] == 'C' && s[i + 1] == 'D' && i < n - 1){
-                num += 400;
-                i++;
-            }
-            else if(s[i] == 'C' && s[i + 1] == 'M' && i < n - 1){
-                num += 900;
-                i++;
-            }
-            else {
-                num += mp[s[i]];
-            }
-            cout<<num<<endl;
         }
-
-        return num;
+        return ans;     
+        
     }
 };
