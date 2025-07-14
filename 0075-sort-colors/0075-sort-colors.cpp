4f2df3faa01141fba9095f2a230ac66104664b37
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-14 14:04:39 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,48 +1,22 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,int s,int mid,int e){
-        
-        int i=s;
-        int j=mid+1;
-        
-        vector<int>ans;
-        while(i<=mid && j<=e){
-            if(nums[i]<=nums[j]){
-                ans.push_back(nums[i]); 
-                i++; 
+    void sortColors(vector<int>& arr) {
+        int n = arr.size();
+        int low = 0, mid = 0, high = n - 1; 
+
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
             }
-            else{
-                ans.push_back(nums[j]);
-                j++;
+            else if (arr[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(arr[mid], arr[high]);
+                high--;
             }
         }
-        
-        while(i<=mid){
-            ans.push_back(nums[i]);
-            i++;
-        }
-        while(j<=e){
-            ans.push_back(nums[j]);
-            j++;
-        }
-        
-        for(int k=s;k<=e;k++){
-            nums[k]=ans[k-s];
-        }
-    }
-    
-    void mergeSort(vector<int>&nums,int s,int e){
-        if(s < e){
-              int mid=s+(e-s)/2;
-              mergeSort(nums,s,mid);
-              mergeSort(nums,mid+1,e);
-              solve(nums,s,mid,e);
-        }
-    }
-    
-    void sortColors(vector<int>& nums) {
-     
-        int n=nums.size();
-        return mergeSort(nums,0,n-1);
     }
 };
