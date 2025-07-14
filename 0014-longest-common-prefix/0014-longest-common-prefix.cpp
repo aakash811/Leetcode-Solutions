8cd31f898f06755e92030f38f4273bec33b81a69
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-15 01:14:00 +0530

    Time: 0 ms (100%), Space: 11.8 MB (66.89%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -2,46 +2,53 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         int n=strs.size();
-        // int count=0;
-        // for(int i=0;i<strs[0].length();i++){
-        //     char x=strs[0][i];
+        int count=0;
+        if(strs.size() == 1){
+            return strs[0];
+        }
+        for(int i=0;i<strs[0].length();i++){
+            char x=strs[0][i];
             
-        //     for(int j=0;j<n;j++){
+            for(int j=0;j<n;j++){
                 
-        //         if(strs[j].length()<i || strs[j][i] !=x){
-        //             return 
-        //         }
-        //     }
-            
-        // }
-        
-        
-        
-        
-        
-        int minLen = INT_MAX;
-        for(int i = 0; i < n; i++){
-            string s = strs[i];
-            for(int j = 0; j < n; j++){
-                if(i == j){
-                    continue;
+                if(strs[j].length()<i || strs[j][i] !=x){
+                    return strs[0].substr(0,count);
                 }
-                string t = strs[j];
-                int len = min(s.size(), t.size());
-                int sz = 0;
-                for(int k = 0; k < len; k++){
-                    if(s[k] != t[k]){
-                        break;
-                    }
-                    else{
-                        sz++;
-                    }
-                }
-                
-                minLen = min(minLen, sz);
             }
+            
+            count++;
+            
         }
         
-        return strs[0].substr(0, minLen);
+        return strs[0].substr(0,count);
+        
+        
+        
+        
+        
+        // int minLen = INT_MAX;
+        // for(int i = 0; i < n; i++){
+        //     string s = strs[i];
+        //     for(int j = 0; j < n; j++){
+        //         if(i == j){
+        //             continue;
+        //         }
+        //         string t = strs[j];
+        //         int len = min(s.size(), t.size());
+        //         int sz = 0;
+        //         for(int k = 0; k < len; k++){
+        //             if(s[k] != t[k]){
+        //                 break;
+        //             }
+        //             else{
+        //                 sz++;
+        //             }
+        //         }
+                
+        //         minLen = min(minLen, sz);
+        //     }
+        // }
+        
+        // return strs[0].substr(0, minLen);
     }
 };
