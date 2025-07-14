3207dceb8ba19b05824434586679eb21eaf71d8d
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-15 01:34:16 +0530

    Time: 5 ms (29.87%), Space: 9.5 MB (98.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,34 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       int n = s.size();
-       int m = t.size();
-
-       if(n != m){
-        return false;
-       } 
-
-      vector<int>hshMap1(256, 0);
-      vector<int>hshMap2(256, 0);
-
-       for(int i = 0; i < n; i++){
-            hshMap1[s[i]]++;
-            hshMap2[t[i]]++; 
-       }
-
-       for(int i = 0; i < hshMap1.size(); i++){
-            if(hshMap1[i] != hshMap2[i]){
-                return false;
-            }
-       }
-
-    //    for(auto it : hshMap1){
-    //     cout<<it<<" ";
-    //    }
-    //    cout<<endl;
-    //    for(auto it : hshMap2){
-    //     cout<<it<<" ";
-    //    }
-       return true;
+        
+        if(s.length()!=t.length()){
+            return false;
+        }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        return s==t;
     }
 };
