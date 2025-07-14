464bac8b98fbe7cf6c4952cafe271fe0c45fbcaa
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-14 23:58:16 +0530

    Time: 7 ms (41.81%), Space: 23.9 MB (31.61%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -7,9 +7,9 @@ public:
         vector<vector<int>>res;
         for(int i = 0; i < n; i++){
             if(res.empty() || res.back()[1] < intervals[i][0]){
-                res.push_back({intervals[i][0], intervals[i][1]});
+                res.push_back(intervals[i]);
             }
-            else if(res.back()[1] >= intervals[i][0] && res.back()[1] <= intervals[i][1]){
+            else if(res.back()[1] >= intervals[i][0] && res.back()[1] < intervals[i][1]){
                 res.back()[1] = intervals[i][1];
             }
         }
