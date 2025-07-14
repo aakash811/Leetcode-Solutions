b6c3f02e9a44ff2c2e0355f55f1c311a96e8e21d
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-15 01:07:13 +0530

    Time: 35 ms (5.56%), Space: 26 MB (5.2%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,16 +1,47 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int n  = strs.size();
-        string pref = "";
-
-        sort(strs.begin(), strs.end());
-        for(int i = 0; i < strs[0].size(); i++){
-            if(strs[0][i] != strs[strs.size() - 1][i]){
-                return pref;
+        int n=strs.size();
+        // int count=0;
+        // for(int i=0;i<strs[0].length();i++){
+        //     char x=strs[0][i];
+            
+        //     for(int j=0;j<n;j++){
+                
+        //         if(strs[j].length()<i || strs[j][i] !=x){
+        //             return 
+        //         }
+        //     }
+            
+        // }
+        
+        
+        
+        
+        
+        int minLen = INT_MAX;
+        for(int i = 0; i < n; i++){
+            string s = strs[i];
+            for(int j = 0; j < n; j++){
+                if(i == j){
+                    continue;
+                }
+                string t = strs[j];
+                int len = min(s.size(), t.size());
+                int sz = 0;
+                for(int k = 0; k < len; k++){
+                    if(s[k] != t[k]){
+                        break;
+                    }
+                    else{
+                        sz++;
+                    }
+                }
+                
+                minLen = min(minLen, sz);
             }
-            pref += strs[0][i];
         }
-        return pref;
+        
+        return strs[0].substr(0, minLen);
     }
 };
