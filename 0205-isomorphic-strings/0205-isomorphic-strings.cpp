f5765aafcba23d93fb9960b0ea815b61cb3185ca
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-15 01:19:29 +0530

    Time: 3 ms (43.11%), Space: 9.4 MB (29.8%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s.size() != t.size()) return false;
-
-        vector<char> mps(256, 0);
-        vector<char> mpt(256, 0); 
-
-        for (int i = 0; i < s.size(); i++) {
-            if (mps[s[i]] == 0 && mpt[t[i]] == 0) {
-                mps[s[i]] = t[i];
-                mpt[t[i]] = s[i]; 
-            } else {
-                if (mps[s[i]] != t[i] || mpt[t[i]] != s[i]) {
-                    return false;
-                }
-            }
+        unordered_map<char, char>ump;
+        unordered_map<char, char>ump2;
+        
+        
+        for(int i = 0; i < s.size(); i++){
+            if(ump.find(s[i]) != ump.end() && ump[s[i]] != t[i]){
+                return false;
+            } 
+            if(ump2.find(t[i]) != ump2.end() && ump2[t[i]] != s[i]){
+                return false;
+            } 
+            ump[s[i]] = t[i];
+            ump2[t[i]] = s[i];
         }
         return true;
     }
