d0f839bdf10a33ea3b4a3399d6f26328dee254b1
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-23 00:15:22 +0530

    Time: 3 ms (42.81%), Space: 9.7 MB (99.9%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,42 +2,36 @@ class Solution {
 public:
     string reverseWords(string s) {
         reverse(s.begin(), s.end());
-        
-        int x = 0;
+
         for(int i = 0; i < s.size(); i++){
             if(s[i] != ' '){
                 s.erase(0, i);
                 break;
             }
         }
-        cout<<s<<endl;
-        
-        
-        int y = 0;
+
         for(int i = 0; i < s.size(); i++){
             if(s[i] == ' ' && s[i + 1] == ' '){
                 s.erase(i, 1);
                 i--;
             }
         }
-        cout<<s<<endl;
-        
+
         for(int i = s.size() - 1; i >= 0; i--){
             if(s[i] != ' '){
-                s.erase(i + 1, s.length() - i + 1);
+                s.erase(i + 1, s.size() - i + 1);
                 break;
             }
-        }
-        cout<<s<<endl;
-        
+        }        
+
         int idx = 0;
         for(int i = 0; i < s.size(); i++){
-            if(s[i] == ' ' ){
+            if(s[i] == ' '){
                 reverse(s.begin() + idx, s.begin() + i);
                 idx = i + 1;
             }
-            if(i==s.size()-1){
-                 reverse(s.begin() + idx, s.begin() + i+1);
+            if(i == s.size() - 1){
+                reverse(s.begin() + idx, s.end());
             }
         }
         return s;
