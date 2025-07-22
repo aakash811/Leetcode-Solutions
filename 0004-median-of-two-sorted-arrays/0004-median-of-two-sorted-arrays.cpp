9ce73b22683695b5ee5ec387eac9e7e70999d08e
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-22 23:33:06 +0530

    Time: 0 ms (100%), Space: 95 MB (93.1%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,38 +2,34 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
-        if (n > m) return findMedianSortedArrays(nums2, nums1);
-        int lo = 0, hi = n;
-        int maxi = (n + m + 1) / 2;
+        if(n > m){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        double lo = 0, hi = n;
+        int sz = (n + m + 1) / 2;
 
         while(lo <= hi){
             int mid1 = (lo + hi) / 2;
-            int mid2 = maxi - mid1;
+            int mid2 = sz - mid1;
+            
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
 
-            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
-            if(mid1 < n){
-                r1 = nums1[mid1];
-            }
-            if(mid2 < m){
-                r2 = nums2[mid2];
-            }
-            if(mid1 - 1 >= 0){
-                l1 = nums1[mid1 - 1];
-            }
-            if(mid2 - 1 >= 0){
-                l2 = nums2[mid2 - 1];
-            }
+            if(mid1 < n) r1 = nums1[mid1];
+            if(mid2 < m) r2 = nums2[mid2];
+            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
 
             if(l1 <= r2 && l2 <= r1){
-                int sz = n + m;
-                if(sz % 2 == 1){
+                int x = n + m;
+                if(x % 2 == 1){
                     return max(l1, l2);
                 }
                 else{
-                    return (double)(max(l1, l2) + min(r1, r2)) / (2.0);
+                    return ((double)(max(l1, l2) + min(r1, r2))) / (2.0);
                 }
             }
-
             else if(l1 > r2){
                 hi = mid1 - 1;
             }
