78cb08e01d17ed6f9b4d4b5a2fb05108e317e3c2
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-24 23:12:09 +0530

    Time: 395 ms (48.51%), Space: 12.1 MB (10.43%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>>& board, string word, int row, int col, int idx, int m, int n, vector<int>& dx, vector<int>& dy, vector<vector<bool>>& vis) {
+    bool solve(vector<vector<char>>& board, string word, vector<int>& dx, vector<int>& dy, vector<vector<int>>& vis, int row, int col, int n, int m, int idx){
         if(idx == word.size()){
             return true;
         }
@@ -10,28 +10,28 @@ public:
             int dr = row + dx[i];
             int dc = col + dy[i];
 
-            if(dr >= 0 && dr < m && dc >= 0 && dc < n && board[dr][dc] == word[idx] && !vis[dr][dc]){
-                if(solve(board, word, dr, dc, idx + 1, m, n, dx, dy, vis)) {
-                    return true; 
+            if(dr < n && dr >= 0 && dc < m & dc >= 0 && board[dr][dc] == word[idx] && vis[dr][dc] == false){
+                if(solve(board, word, dx, dy, vis, dr, dc, n, m, idx + 1)){
+                    return true;
                 }
             }
         }
-
         vis[row][col] = false;
         return false;
     }
-    
+
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
+        int n = board.size();
+        int m = board[0].size();
+        
         vector<int>dx = {-1, 0, 1, 0};
         vector<int>dy = {0, -1, 0, 1};
 
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(board[i][j] == word[0]){
-                    vector<vector<bool>>vis(m, vector<bool>(n, false));
-                    if(solve(board, word, i, j, 1, m, n, dx, dy, vis)){
+                    vector<vector<int>>vis(n, vector<int>(m, false));
+                    if(solve(board, word, dx, dy, vis, i, j, n, m, 1)){
                         return true;
                     }
                 }
