6f0feca595d320bf3df6deb61718dad37e25687e
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-24 23:22:00 +0530

    Time: 408 ms (47.91%), Space: 12.1 MB (10.43%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    bool solve(vector<vector<char>>& board, string word, vector<int>& dx, vector<int>& dy, vector<vector<int>>& vis, int row, int col, int n, int m, int idx){
+    bool solve(vector<vector<char>>& board, vector<vector<bool>>& vis, string word, vector<int>& dx, vector<int>& dy, int row, int col, int n, int m, int idx){
         if(idx == word.size()){
             return true;
         }
-
         vis[row][col] = true;
-        for(int i = 0; i < 4; i++){
-            int dr = row + dx[i];
-            int dc = col + dy[i];
 
-            if(dr < n && dr >= 0 && dc < m & dc >= 0 && board[dr][dc] == word[idx] && vis[dr][dc] == false){
-                if(solve(board, word, dx, dy, vis, dr, dc, n, m, idx + 1)){
+        for(int i = 0; i < 4; i++){
+            int drow = row + dx[i];
+            int dcol = col + dy[i];
+
+            if(drow < n && drow >= 0 && dcol < m && dcol >= 0 && board[drow][dcol] == word[idx] && vis[drow][dcol] == false){
+                if(solve(board, vis, word, dx, dy, drow, dcol, n, m, idx + 1)){
                     return true;
                 }
             }
@@ -23,15 +23,15 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
-        
+
         vector<int>dx = {-1, 0, 1, 0};
         vector<int>dy = {0, -1, 0, 1};
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(board[i][j] == word[0]){
-                    vector<vector<int>>vis(n, vector<int>(m, false));
-                    if(solve(board, word, dx, dy, vis, i, j, n, m, 1)){
+                    vector<vector<bool>>vis(n, vector<bool>(m, false));
+                    if(solve(board, vis, word, dx, dy, i, j, n, m, 1)){
                         return true;
                     }
                 }
