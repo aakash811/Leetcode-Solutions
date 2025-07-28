73810555fd52468b77f17871aac5cb60ea59b68d
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-28 16:18:55 +0530

    Time: 12 ms (34.7%), Space: 55.8 MB (57.09%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.cpp
+++ 0355-design-twitter/0355-design-twitter.cpp
@@ -1,46 +1,47 @@
 class Twitter {
 public:
-    unordered_map<int, vector<pair<int, int>>>posts;
-    unordered_map<int, unordered_set<int>>follows;
+    unordered_map<int, unordered_set<int>>followList;
+    unordered_map<int, vector<pair<int,int>>>postList;
     int time;
     Twitter() {
         time = 0;
     }
     
     void postTweet(int userId, int tweetId) {
-        posts[userId].push_back({time, tweetId});
+        postList[userId].push_back({time, tweetId});
         time++;
     }
     
     vector<int> getNewsFeed(int userId) {
         priority_queue<pair<int, int>>pq;
 
-        for(auto it : posts[userId]){
+        for(auto it : postList[userId]){
             pq.push(it);
         }
 
-        for(auto it : follows[userId]){
-            for(auto jt : posts[it]){
+        for(auto it : followList[userId]){
+            for(auto jt : postList[it]){
                 pq.push(jt);
             }
         }
-        
-        vector<int>news;
+
         int cnt = 10;
-        while(!pq.empty() && cnt){
-            news.push_back(pq.top().second);
+        vector<int>res;
+
+        while(cnt && !pq.empty()){
+            res.push_back(pq.top().second);
             pq.pop();
             cnt--;
         }
-        return news;
+        return res;
     }
     
     void follow(int followerId, int followeeId) {
-        follows[followerId].insert(followeeId);
+        followList[followerId].insert(followeeId);
     }
     
     void unfollow(int followerId, int followeeId) {
-        follows[followerId].erase(followeeId);
+        followList[followerId].erase(followeeId);
     }
 };
 
