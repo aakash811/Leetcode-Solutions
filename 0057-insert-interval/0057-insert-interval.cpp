d449143e2017447bd635366fa73a5e219df960b8
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-29 17:56:16 +0530

    Time: 0 ms (100%), Space: 21.9 MB (20.04%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,35 +2,35 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int n = intervals.size();
-        vector<vector<int>>res;
-        int st = newInterval[0];
-        int en = newInterval[1];
+        int start = newInterval[0];
+        int end = newInterval[1];
         bool flag = false;
 
+        vector<vector<int>>res;
         for(int i = 0; i < n; i++){
-            int x = intervals[i][0];
-            int y = intervals[i][1];
+            int intervalStart = intervals[i][0];
+            int intervalEnd = intervals[i][1];
 
-            if(st > y || flag){
-                res.push_back({x, y});
+            if(intervalEnd < start || flag){
+                res.push_back({intervalStart, intervalEnd});
                 continue;
             }
-            st = min(st, x);
-            if(en < x){
-                res.push_back({st, en});
-                res.push_back({x, y});
+            start = min(start, intervalStart);
+            if(intervalStart > end){
+                res.push_back({start, end});
+                res.push_back({intervalStart, intervalEnd});
                 flag = true;
                 continue;
             }
-            if(en <= y){
-                res.push_back({st, y});
+            else if(intervalEnd >= end){
+                res.push_back({start, intervalEnd});
                 flag = true;
                 continue;
             }
         }
 
         if(!flag){
-            res.push_back({st, en});
+            res.push_back({start, end});
         }
         return res;
     }
