8b04e48aba809e76b36a685cc15bb3dec5063ed5
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-29 18:57:33 +0530

    Time: 0 ms (100%), Space: 19.2 MB (13.98%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -15,21 +15,21 @@ public:
         if(!root){
             return 0;
         }
-        queue<TreeNode*>q;
-        q.push({root});
-
         int hgt = 0;
+        queue<TreeNode*>q;
+        q.push(root);
+
         while(!q.empty()){
             int sz = q.size();
             for(int i = 0; i < sz; i++){
                 TreeNode* node = q.front();
                 q.pop();
 
-                if(node -> left){
-                    q.push(node -> left);
+                if(node->left){
+                    q.push(node->left);
                 }
-                if(node -> right){
-                    q.push(node -> right);
+                if(node->right){
+                    q.push(node->right);
                 }
             }
             hgt++;
