afbd2f29390d0ab9700d0a2466b0f7043d743272
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-29 18:14:23 +0530

    Time: 3 ms (89.58%), Space: 24 MB (23.22%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        sort(intervals.begin(), intervals.end());
-
         vector<vector<int>>res;
-
+        sort(intervals.begin(), intervals.end());
+        
         for(int i = 0; i < n; i++){
             if(res.empty() || res.back()[1] < intervals[i][0]){
                 res.push_back(intervals[i]);
@@ -14,7 +13,6 @@ public:
                 res.back()[1] = intervals[i][1];
             }
         }
-
         return res;
     }
 };
