e8187877cc27eb3d0f3dd17e3145f268287ede06
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-07-30 15:07:27 +0530

    Time: 3 ms (40.6%), Space: 16.4 MB (43.87%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,21 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> mp;
-        queue<pair<pair<int,int>, TreeNode*>> q;
-        vector<vector<int>> res;
+        map<int, map<int, multiset<int>>>mp;
+        queue<pair<pair<int, int>, TreeNode*>>q;
 
         q.push({{0, 0}, root});
 
-        while(!q.empty()) {
+        while(!q.empty()){
             auto it = q.front();
             q.pop();
-            int x = it.first.first;  
-            int y = it.first.second;  
+            
+            int x = it.first.first;
+            int y = it.first.second;
             TreeNode* node = it.second;
 
             mp[x][y].insert(node->val);
-
             if(node->left){
                 q.push({{x - 1, y + 1}, node->left});
             }
@@ -24,16 +34,16 @@ public:
             }
         }
 
-        for(auto& it : mp){
-            vector<int> ds;
-            for(auto& jt : it.second){
-                for(int val : jt.second){
-                    ds.push_back(val);
+        vector<vector<int>>res;
+        for(auto it : mp){
+            vector<int>ds;
+            for(auto jt : it.second){
+                for(auto kt : jt.second){
+                    ds.push_back(kt);
                 }
             }
             res.push_back(ds);
         }
-
         return res;
     }
 };
