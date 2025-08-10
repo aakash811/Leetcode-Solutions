884da6599aaaa1ec3bac98785101985991075422
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-08-10 15:56:12 +0530

    Time: 1 ms (91.83%), Space: 15.3 MB (57.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    map<pair<int,int>,int>dp;
-    int solve(int idx, int sum, vector<int>& nums, int target){
+    int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        if(sum == target && idx == n){
-            return 1;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum - target < 0 || (sum - target) % 2 != 0){
+            return false;
         }
-        if(idx >= n){
-            return 0;
+
+        int s1 = (sum - target) / 2;     
+
+        vector<vector<int>>dp(n, vector<int>(s1 + 1, 0));
+        if(nums[0] == 0){
+            dp[0][0] = 2;
         }
-        if(dp.find({idx,sum}) != dp.end()){
-            return dp[{idx,sum}];
+        else{
+            dp[0][0] = 1;
+
+            if(nums[0] <= s1){
+                dp[0][nums[0]] = 1;
+            }
         }
-        int add = solve(idx + 1, sum + nums[idx], nums, target);
-        int sub = solve(idx + 1, sum - nums[idx], nums, target);
-        return dp[{idx,sum}] = add + sub;
-    }
-    int findTargetSumWays(vector<int>&nums,int target) {
-        return solve(0,0,nums,target);
+
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= s1; j++){
+                int notTake = dp[i - 1][j];
+                int take = 0;
+                if(nums[i] <= j){
+                    take = dp[i - 1][j - nums[i]];
+                }
+                dp[i][j] = take + notTake;
+            }
+        }                                 
+        return dp[n - 1][s1];     
     }
 };
