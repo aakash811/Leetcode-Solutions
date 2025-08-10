9fd6c0f34ef5a6c46e16a4d87913fcd2f6fef3de
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-08-10 16:11:55 +0530

    Time: 4 ms (79.24%), Space: 15.4 MB (52.23%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -3,36 +3,35 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int sum = accumulate(nums.begin(), nums.end(), 0);
+
         if(sum - target < 0 || (sum - target) % 2 != 0){
-            return false;
+            return 0;
         }
 
-        int s1 = (sum - target) / 2;     
+        int tar = (sum - target) / 2;
+        vector<vector<int>>dp(n, vector<int>(tar + 1, 0));
 
-        vector<int>dp(s1 + 1, 0);
         if(nums[0] == 0){
-            dp[0] = 2;
+            dp[0][0] = 2;
         }
         else{
-            dp[0] = 1;
-
-            if(nums[0] <= s1){
-                dp[nums[0]] = 1;
+            dp[0][0] = 1;
+            if(nums[0] <= tar){
+                dp[0][nums[0]] = 1;
             }
         }
 
         for(int i = 1; i < n; i++){
-            vector<int>prev(s1 + 1, 0);
-            for(int j = 0; j <= s1; j++){
-                int notTake = dp[j];
+            for(int j = 0; j <= tar; j++){
+                int notTake = dp[i - 1][j];
                 int take = 0;
                 if(nums[i] <= j){
-                    take = dp[j - nums[i]];
+                    take = dp[i - 1][j - nums[i]];
                 }
-                prev[j] = take + notTake;
+                dp[i][j] = take + notTake;
             }
-            dp = prev;
-        }                                 
-        return dp[s1];     
+        }
+
+        return dp[n - 1][tar];
     }
 };
