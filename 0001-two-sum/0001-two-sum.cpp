f2579cbdf833c487da9c9dc405f7ab4a1102d309
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-08-16 00:57:03 +0530

    Time: 0 ms (100%), Space: 14.9 MB (36.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,11 +3,12 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         unordered_map<int, int>ump;
+        int sum = 0;
 
         for(int i = 0; i < n; i++){
-            int comp = target - nums[i];
-            if(ump.find(comp) != ump.end()){
-                return {ump[comp], i};
+            int complement = target - nums[i];
+            if(ump.find(complement) != ump.end()){
+                return {i, ump[complement]};
             }
             ump[nums[i]] = i;
         }
