bc2fdd8cdd8879e7c018d6fce9a427443c8333d8
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-08-28 22:27:31 +0530

    Time: 3 ms (73.3%), Space: 18.1 MB (78.91%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -2,40 +2,40 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
-        vector<int>inDegree(numCourses, 0);
+        vector<int>inDeg(numCourses);
 
         for(int i = 0; i < prerequisites.size(); i++){
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
 
             adj[v].push_back(u);
-            inDegree[u]++;
+            inDeg[u]++;
         }
 
         queue<int>q;
+        vector<int>topo;
         for(int i = 0; i < numCourses; i++){
-            if(inDegree[i] == 0){
+            if(inDeg[i] == 0){
                 q.push(i);
             }
         }
 
-        vector<int>topo;
         while(!q.empty()){
             int node = q.front();
-            topo.push_back(node);
             q.pop();
+            topo.push_back(node);
 
             for(auto it : adj[node]){
-                inDegree[it]--;
-                if(inDegree[it] == 0){
+                inDeg[it]--;
+                if(inDeg[it] == 0){
                     q.push(it);
                 }
             }
         }
 
-        if(topo.size() != numCourses){
-            return {};
+        if(topo.size() == numCourses){
+            return topo;
         }
-        return topo;
+        return {};
     }
 };
