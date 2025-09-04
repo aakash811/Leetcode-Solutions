5fab9e6477541fff0a347ef2b759f4e3afaf9252
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-09-04 20:08:10 +0530

    Time: 111 ms (32.25%), Space: 26.3 MB (30.11%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,37 +1,72 @@
 class Solution {
 public:
-bool isPalin(string &s){
-    int i=0;
-    int j=s.length()-1;
-    
-    while(i<j){
-        if(s[i]!=s[j]){
-            return false;
+    bool isPalindrome(string s, int i, int j){
+        while(i <= j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
         }
-        i++;
-        j--;
-    } 
-    return true;
-}
 
-   
+        return true;
+    }
+
+    // string solve(string s, int i, int j, vector<vector<string>>& dp){
+    //     if(i > j){
+    //         return "";
+    //     }
+
+    //     if(i == j){
+    //         return string(1, s[i]);
+    //     }
+
+    //     if(dp[i][j] != ""){
+    //         return dp[i][j];
+    //     }
+
+    //     if(isPalindrome(s, i, j)){
+    //         return dp[i][j] = s.substr(i, j - i + 1);
+    //     }
+
+    //     string left = solve(s, i + 1, j, dp);
+    //     string right = solve(s, i, j - 1, dp);
+
+    //     return dp[i][j] = (left.size() >= right.size()) ? left : right;
+    // }
 
     string longestPalindrome(string s) {
-        int n=s.length();
-        string maxi="";
-        
-        for(int i=0;i<n;i++){
-            string temp="";
-            for(int j=i;j<n;j++){
-                temp+=s[j];
-                if(isPalin(temp)){
-                    if(temp.length()>maxi.length()){
-                        maxi=temp;
+        int n = s.size();
+        vector<vector<bool>>dp(n, vector<bool>(n, false));
+        int start = 0, maxLen = 1;
+
+        for(int i = 0; i < n; i++){
+            dp[i][i] = true;
+        }
+
+        for(int i = 0; i < n - 1; i++){
+            if(s[i] == s[i + 1]){
+                dp[i][i + 1] = true;
+                start = i;
+                maxLen = 2;
+            }
+        }
+
+        for(int len = 3; len <= n; len++){
+            for(int i = 0; i  <= n - len; i++){
+                int j = i + len - 1;
+
+                if(s[i] == s[j] && dp[i + 1][j - 1]){
+                    dp[i][j] = true;
+
+                    if(len > maxLen){
+                        start = i;
+                        maxLen = len;
                     }
                 }
             }
         }
-        
-        return maxi;
+        return s.substr(start, maxLen);
+        // return solve(s, 0, n - 1, dp);
     }
 };
