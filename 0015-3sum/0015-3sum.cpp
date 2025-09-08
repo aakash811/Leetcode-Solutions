dfc2a2c080c0e64e6471c0a9bd6eea5a76550032
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2025-09-08 20:08:58 +0530

    Time: 132 ms (13.96%), Space: 41.8 MB (12.05%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -28,6 +28,9 @@ public:
                     while(j < k && nums[j] == nums[j-1]) {
                         j++;
                     } 
+                    while(j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
                 }
             }
         }
