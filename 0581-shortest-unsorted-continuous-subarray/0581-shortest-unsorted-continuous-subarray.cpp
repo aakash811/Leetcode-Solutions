f3b214e42e3cb508d156e9cd8c3279a3bbe30a26
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2026-07-27 10:55:11 +0530

    Time: 0 ms (100%), Space: 30.4 MB (74.55%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -3,60 +3,24 @@ public:
     int findUnsortedSubarray(vector<int>& nums) {
         int n = nums.size();
 
-        int i = 0;
-        bool flag = false;
-        while(i < n - 1){
-            if(i > 0 && nums[i] == nums[i - 1]){
-                i++;
-                continue;
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+
+        int x = -1, y = -1;
+        for(int i = 0; i < n; i++){
+            maxi = max(maxi, nums[i]);
+            if(nums[i] < maxi){
+                y = i;
             }
-            int j = i + 1;
-            while(j < n){
-                if(j >= i + 1 && nums[j] == nums[j - 1]){
-                    j++;
-                    continue;
-                }
-                if(j < n && nums[i] <= nums[j]){
-                    j++;
-                }
-                else{
-                    flag = true;
-                    break;
-                }
-            }
-            if(flag){
-                break;
-            }
-            i++;
         }
 
-        int k = n - 1 ;
-        bool flag1 = false;
-        while(k >= 0){
-            if(k < n - 1 && nums[k] == nums[k + 1]){
-                k--;
-                continue;
+        for(int i = n - 1; i >= 0; i--){
+            mini = min(mini, nums[i]);
+            if(nums[i] > mini){
+                x = i;
             }
-            int l = k - 1;
-            while(l >= 0){
-                if(l >= k - 1 && nums[l] == nums[l + 1]){
-                    l--;
-                    continue;
-                }
-                if(l >= 0 && nums[k] >= nums[l]){
-                    l--;
-                }
-                else{
-                    flag1 = true;
-                    break;
-                }
-            }
-            if(flag1){
-                break;
-            }
-            k--;
         }
 
-        return (flag && flag1) ? k - i + 1 : 0;
+        return (x != -1 && y != -1) ? y - x + 1 : 0;
     }
 };
