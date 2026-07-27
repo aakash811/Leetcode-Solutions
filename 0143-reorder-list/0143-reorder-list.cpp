26d6f2ae2c26b0db7000a99a755d79aa04662a38
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2026-07-27 20:44:08 +0530

    Time: 0 ms (100%), Space: 22.8 MB (78.47%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,77 +11,36 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if(head == NULL || head->next == NULL){
-            return;
+        if(!head)return;
+
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
         }
 
-        ListNode* slow = head;
-        ListNode* fast = head;
+        ListNode* second = slow->next;
+        slow->next=nullptr;
+        ListNode* prev=nullptr;
 
-        while(fast != NULL && fast->next != NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        ListNode* tempSlow = head;
-        while(tempSlow->next != slow){
-            tempSlow = tempSlow->next;
-        }
-        tempSlow->next = NULL;
-        if(fast == NULL){
-            ListNode* tempFast = slow;
-            while(tempFast->next != NULL){
-                tempFast = tempFast->next;
-            }
-            fast = tempFast;
+        while(second){
+            ListNode* temp=second->next;
+            second->next=prev;
+            prev=second;
+            second=temp;
         }
 
-        ListNode* nxt = slow->next;
-        ListNode* prev = NULL;
+        ListNode* first=head;
+        second=prev;
 
-        while(slow != NULL){
-            slow->next = prev;
-            prev = slow;
-            slow = nxt;
-            if(nxt != NULL){
-                nxt = nxt->next;
-            }
+        while(second){
+            ListNode* temp1=first->next;
+            ListNode* temp2=second->next;
+            first->next=second;
+            second->next=temp1;
+            first=temp1;
+            second=temp2;
         }
-
-        ListNode* pfHead = head;
-        ListNode* pfFast = fast;
-        while(pfHead != NULL){
-            cout<<"pfHead-> "<<pfHead->val<<endl;
-            pfHead = pfHead->next;
-        }
-        while(pfFast != NULL){
-            cout<<"pfFast-> "<<pfFast->val<<endl;
-            pfFast = pfFast->next;
-        }
-
-        struct ListNode* result = new ListNode(); 
-        ListNode* res = result;
-        int count = 0;
-        while(head != NULL && fast != NULL){
-            if(count % 2 == 0){
-                res->next = head;
-                head = head->next;
-            }
-            else{
-                res->next = fast;
-                fast = fast->next;
-            }
-            res = res->next;
-            count++;
-        }
-
-        if(head != NULL){
-            res->next = head;
-        }
-        else if(fast != NULL){
-            res->next = fast;
-        }
-
-        head = result->next;
     }
 };
