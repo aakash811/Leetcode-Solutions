8a0a38ef75684487187c451771773850bec7cd2c
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2026-07-30 20:55:42 +0530

    Time: 0 ms (100%), Space: 79.6 MB (99.97%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -2,21 +2,46 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
-        unordered_map<int, int>ump;
 
         int left = 0;
         int right = 0;
+
+        int type1 = -1;
+        int type2 = -1;
+
+        int idx1 = -1;
+        int idx2 = -1;
+
         int maxLen = 0;
 
         while(right < n){
-            ump[fruits[right]]++;
-
-            while(ump.size() > 2){
-                ump[fruits[left]]--;
-                if(ump[fruits[left]] == 0){
-                    ump.erase(fruits[left]);
+            if(fruits[right] == type1){
+                idx1 = right;
+            }
+            else if(fruits[right] == type2){
+                idx2 = right;
+            }
+            else{
+                if(type1 == -1){
+                    type1 = fruits[right];
+                    idx1 = right;
                 }
-                left++;
+                else if(type2 == -1){
+                    type2 = fruits[right];
+                    idx2 = right;
+                }
+                else{
+                    if(idx1 < idx2){
+                        left = idx1 + 1;
+                        type1 = fruits[right];
+                        idx1 = right;
+                    }
+                    else{
+                        left = idx2 + 1;
+                        type2 = fruits[right];
+                        idx2 = right;
+                    }
+                }
             }
             maxLen = max(maxLen, right - left + 1);
             right++;
