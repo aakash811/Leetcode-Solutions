96026294c6ea9d37e676cea991c991ad506ba9cd
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2026-08-03 18:07:37 +0530

    Time: 4 ms (17.54%), Space: 22.1 MB (12.68%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,36 +2,57 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int n = intervals.size();
-        int start = newInterval[0];
-        int end = newInterval[1];
-        bool flag = false;
-
-        vector<vector<int>>res;
-        for(int i = 0; i < n; i++){
-            int intervalStart = intervals[i][0];
-            int intervalEnd = intervals[i][1];
-
-            if(intervalEnd < start || flag){
-                res.push_back({intervalStart, intervalEnd});
-                continue;
-            }
-            start = min(start, intervalStart);
-            if(intervalStart > end){
-                res.push_back({start, end});
-                res.push_back({intervalStart, intervalEnd});
-                flag = true;
-                continue;
-            }
-            else if(intervalEnd >= end){
-                res.push_back({start, intervalEnd});
-                flag = true;
-                continue;
-            }
+        if(n == 0){
+            return {newInterval};
         }
+        sort(intervals.begin(), intervals.end());
 
-        if(!flag){
-            res.push_back({start, end});
+       vector<vector<int>>merged;
+       bool done = false;
+
+       if(intervals[0][0] > newInterval[0]){
+        merged.push_back(newInterval);
+        done = true;
+       }
+       else{
+        merged.push_back(intervals[0]);
+       }
+
+       for(int i = 0; i < n; i++){
+        if(!done && i == 0){
+            continue;
         }
-        return res;
+        vector<int>& currentInterval = merged.back();
+        vector<int> nextInterval = intervals[i];
+
+        if(!done && nextInterval[0] > newInterval[0]){
+            if(currentInterval[1] >= newInterval[0]){
+                currentInterval[1] = max(currentInterval[1], newInterval[1]);
+            }
+            else{
+                merged.push_back(newInterval);
+            }
+            done = true;
+        }
+        
+        vector<int>& currInterval = merged.back();
+        if(currInterval[1] >= nextInterval[0]){
+            currInterval[1] = max(currInterval[1], nextInterval[1]);
+        }
+        else{
+            merged.push_back(nextInterval);
+        }
+       }
+
+       if(!done){
+        if(merged.back()[1] >= newInterval[0]){
+            merged.back()[1] = max(merged.back()[1], newInterval[1]);
+        }
+        else{
+            merged.push_back(newInterval);
+        }
+       }
+
+       return merged;
     }
 };
