6792455e7e7fb40dce2d99c5f71aaabdfe7b33dc
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2026-08-03 12:27:19 +0530

    Time: 3 ms (88.8%), Space: 24.9 MB (10.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,17 +3,27 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>>res;
 
-        for(int i = 0; i < n; i++){
-            if(res.empty() || res.back()[1] < intervals[i][0]){
-                res.push_back(intervals[i]);
+        vector<vector<int>>merged;
+        merged.push_back(intervals[0]);
+
+        for(int i = 1; i < n; i++){
+            vector<int>& currInterval = merged.back();
+            vector<int> nextInterval = intervals[i];
+
+            // cout<<"CurrInterval-> "<<currInterval[0]<<" "<<currInterval[1]<<endl;
+            // cout<<"NextInterval-> "<<nextInterval[0]<<" "<<nextInterval[1]<<endl;
+            // cout<<"MaxEnd-> "<<max(currInterval[1], nextInterval[1])<<endl;
+
+            if(currInterval[1] >= nextInterval[0]){
+                currInterval[1] = max(currInterval[1], nextInterval[1]);
+                // cout<<"CurrInterval after maxEnd-> "<<currInterval[0]<<" "<<currInterval[1]<<endl;
             }
-            else if(res.back()[1] >= intervals[i][0] && res.back()[1] < intervals[i][1]){
-                res.back()[1] = intervals[i][1];
+            else{
+                merged.push_back(nextInterval);
             }
         }
 
-        return res;
+        return merged;
     }
 };
