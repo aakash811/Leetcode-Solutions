3c775da52e34330eb719754c6e0bd391e71ac92a
Aakash Borse <125889687+aakash811@users.noreply.github.com>
2026-08-09 17:36:38 +0530

    Time: 48 ms (19.3%), Space: 13.7 MB (43.8%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>>dp(n + 1, vector<int>(n + 1, 0));
+
+        vector<int>suffix(n + 1, 0);
+        for(int i = n - 1; i >= 0; i--){
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        for(int i = 0; i <= n; i++){
+            dp[i][n] = suffix[i];
+        }
+
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 1; j--){
+                for(int k = 1; k <= 2 * j && i + k <= n; k++){
+                    dp[i][j] = max(dp[i][j], suffix[i] - dp[i + k][max(j, k)]);
+                }
+            }
+        }
+
+        return dp[0][1];
+    }
+};
